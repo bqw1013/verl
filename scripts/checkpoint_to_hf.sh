5b4805b799ed067129b2b465d0168908fdd890e8
bqw <qiangweibai1013@gmail.com>
2025-07-02 21:25:34 +0800

    xxx

--- scripts/checkpoint_to_hf.sh
+++ scripts/checkpoint_to_hf.sh
@@ -1,5 +1,5 @@
 python scripts/model_merger.py merge \
     --backend fsdp \
     --hf_model_path /root/autodl-fs/models/Qwen/Qwen2___5-Math-1___5B \
-    --local_dir /root/autodl-tmp/code/verl/checkpoints/pass_at_k_improvement_experiment/pkpo_without_kl_entropy/global_step_240/actor \
-    --target_dir /root/autodl-fs/models/qwen2.5_math_1.5b_passk_pkpo_without_kl_entropy/step_240
+    --local_dir /root/autodl-tmp/code/verl/checkpoints/pass_at_k_improvement_experiment/pkpo_entropy_0.0005/global_step_240/actor \
+    --target_dir /root/autodl-fs/models/qwen2.5_math_1.5b_passk_pkpo_entropy_0.0005/step_240
