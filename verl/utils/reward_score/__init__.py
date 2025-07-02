88adb0555cb8bd364bebfcafd30b20a433da71b1
bqw <qiangweibai1013@gmail.com>
2025-07-03 02:33:44 +0800

    modify reward compute func

--- verl/utils/reward_score/__init__.py
+++ verl/utils/reward_score/__init__.py
@@ -19,10 +19,10 @@ def _default_compute_score(data_source, solution_str, ground_truth, extra_info=N
         from . import gsm8k
 
         res = gsm8k.compute_score(solution_str, ground_truth)
-    elif data_source == 'simplelr_qwen':
-        from . import hf_math_verify
-        res = hf_math_verify.compute_score(solution_str, ground_truth)
-        res = res["score"]
+    elif data_source == "simplelr_qwen":
+        from . import math_verify
+
+        res = math_verify.compute_score(solution_str, ground_truth)
     elif data_source in ["lighteval/MATH", "DigitalLearningGmbH/MATH-lighteval"]:
         from . import math
 
